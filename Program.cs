e8fa42c71483d403840a730fb600c5d413af34a1
David Ebbo <david.ebbo@gmail.com>
2016-07-28 18:21:25 -0700

    Init

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFirstByteOfFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = args.Length > 0 ? args[0] : @"D:\home\LogFiles\Application";
+
+            while (true)
+            {
+                foreach (var file in Directory.EnumerateFiles(folder, "*.log"))
+                {
+                    using (var stream = File.Open(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    {
+                        int b = stream.ReadByte();
+                    }
+                }
+
+                System.Threading.Thread.Sleep(200);
+            }
+        }
+    }
+}
